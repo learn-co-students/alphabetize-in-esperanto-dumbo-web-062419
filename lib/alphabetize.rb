e1d77f8ddf7c1a59ebe6d93fcf8d657a19ae46ff
Olga Rosas <olga.rosas@gmail.com>
2019-06-18 11:39:39 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,6 @@ require 'pry'
 
 def alphabetize(arr)
   # code here
-  def alphabetize(arr)
   esperanto_alphabet = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
   arr.sort_by do |word|
     word.split('').collect do |letter|
