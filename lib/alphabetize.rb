c8482c1aea95fff180484809760f1af683fc60d2
Olga Rosas <olga.rosas@gmail.com>
2019-06-18 11:38:58 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
 def alphabetize(arr)
   # code here
+  def alphabetize(arr)
+  esperanto_alphabet = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
+  arr.sort_by do |word|
+    word.split('').collect do |letter|
+      esperanto_alphabet.index(letter)
+    end
+  end
+end
 end
