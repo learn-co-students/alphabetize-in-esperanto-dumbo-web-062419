28009a696c47cd8e48ef63177fe0523ab66267ae
Aliciea D Suechin <adr330@nyu.edu>
2019-06-20 00:19:01 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,11 @@
 def alphabetize(arr)
   # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+arr.sort(esperanto_alphabet)
+arr.sort_by do |esperanto_alphabet|
+  esperanto_alphabet.split(" ")
+  esperanto_alphabet = array
+#     esperanto_array.index(individual_letters)
+#   end
+# end   
 end
