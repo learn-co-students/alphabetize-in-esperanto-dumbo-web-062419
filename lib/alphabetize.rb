f3aadb6c7b436d2bc9417d2ef8603bae0a3aa14c
Aliciea D Suechin <adr330@nyu.edu>
2019-06-20 00:31:34 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,11 +1,12 @@
 def alphabetize(arr)
   # code here
-  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 arr.sort(esperanto_alphabet)
 arr.sort_by do |esperanto_alphabet|
   esperanto_alphabet.split(" ")
   esperanto_alphabet = array
-#     esperanto_array.index(individual_letters)
-#   end
-# end   
+  esperanto_array.index(individual_letters)
+  
+  end
 end
+  end 
