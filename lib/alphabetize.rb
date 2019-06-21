d4c5fcd312c13327469589ada534b3c41b5f652c
github username <github email address>
2019-06-21 16:29:23 -0400

    Testing first iteration

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,23 @@
+# binding.pry
+require 'pry'
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+# Using bubble sort
+# Credits: https://stackoverflow.com/questions/11091434/using-the-bubble-sort-method-for-an-array-in-ruby
 def alphabetize(arr)
   # code here
+  organized_arr = []
+
+
+  # ESPERANTO_ALPHABET.split("").each_with_index do |esp_letter, esp_letter_index|
+
+  # end # End ESPERANTO_ALPHABET iteration
+
+  arr.each do |phrase|
+    organized_arr << phrase
+
+  end # End arr iteration
+
+  organized_arr
 end
